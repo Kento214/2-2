365248b1faf3733fe14e8982124f465aa38252f6
Kento214 <136420042+Kento214@users.noreply.github.com>
2023-07-13 00:39:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
-st.title("SUPER KAZUMI BROS")#タイトル入力
-st.write("暗号を入力しよう")#文入力
+st.title("SUPER KAZUMI BROS.")#タイトル入力
+st.write("暗号を入力しよう（２回までしか入力できません")#文入力
 
 user_input = st.text_input("暗号")#テキストボックス挿入取得
 
@@ -11,18 +11,23 @@ image = Image.open("IMG_2167.PNG")
 
 mistake=Image.open("IMG_2175.PNG") 
 
-if submit_btn and user_input=="2315":#送信ボタンを押して暗号が合っていれば
+if "increment" not in st.session_state:
+    st.session_state["increment"] = 0
+
+
+if submit_btn and user_input=="2315"　and st.session_state["increment"]<=2:#送信ボタンを押して暗号が合っていれば
    st.image(image,width=700)#救出画像を表示する
    st.write("ゲーム　クリア！")
    st.write("おめでとう！")
    st.balloons()#風船を飛ばす
 
-elif submit_btn:#違うとき
+elif submit_btn　and st.session_state["increment"]==0:#違うとき
      st.image(mistake,width=700)#失敗画像を表示する
-     
+     st.session_state["increment"] += 1
 
 
-
+elif submit_btn and st.session_state["increment"] >=1:
+     st.write("GAMEOVER")
 
 
 
