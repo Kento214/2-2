71d133709895c4cde26724e47306ee8ec087c1ad
Kento214 <136420042+Kento214@users.noreply.github.com>
2023-07-13 00:42:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -15,13 +15,13 @@ if "increment" not in st.session_state:
     st.session_state["increment"] = 0
 
 
-if submit_btn and user_input=="2315"　and st.session_state["increment"]<=2:#送信ボタンを押して暗号が合っていれば
+if submit_btn and user_input=="2315"and st.session_state["increment"]<=2:#送信ボタンを押して暗号が合っていれば
    st.image(image,width=700)#救出画像を表示する
    st.write("ゲーム　クリア！")
    st.write("おめでとう！")
    st.balloons()#風船を飛ばす
 
-elif submit_btn　and st.session_state["increment"]==0:#違うとき
+elif submit_btn and st.session_state["increment"]==0:#違うとき
      st.image(mistake,width=700)#失敗画像を表示する
      st.session_state["increment"] += 1
 
